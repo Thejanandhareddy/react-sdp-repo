0313be1f799f566926371b3c1f45f5a7d4fcb81d
thejanandha <2500032304@kluniversity.in>
2026-02-23 15:19:24 +0530

    project Ver2.o

--- src/pages/CustomerLogin.jsx
+++ src/pages/CustomerLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './style.css';
+import './Style.css';
 
 const CustomerLogin = () => {
   const navigate = useNavigate();
