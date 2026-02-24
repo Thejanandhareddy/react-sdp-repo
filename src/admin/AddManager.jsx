9e2201dabde1cb388c0e6a18d8d69588652cb384
thejanandha <2500032304@kluniversity.in>
2026-02-24 12:44:25 +0530

    Project V3.0

--- src/admin/AddManager.jsx
+++ src/admin/AddManager.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../pages/style.css';
+import '../pages/Style.css';
 
 export default function AddManager() {
   const [formData, setFormData] = useState({
